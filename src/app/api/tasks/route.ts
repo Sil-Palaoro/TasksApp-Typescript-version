8173cec053988d647b6a7ceb4e2aa82d06953bbc
Sil_Palaoro <silvinapalaoro@gmail.com>
2025-09-24 12:42:13 -0300

    Add types and clean code in tasks API

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -2,69 +2,71 @@ import { verifyJWT } from '@/lib/jwt';
 import prisma from '@/lib/prisma';
 
 
-export async function GET(request) {
-  // Verificar el token de autorización 
+const verifyToken = (request: Request) => {
   const authHeader = request.headers.get('authorization');
   const token = authHeader?.split(' ')[1];
+  return token;
+}
 
-  // Si no hay token, retornar error 401 
+async function getPayload(token: string | undefined) {
   const payload = await verifyJWT(token);
+
   if (!payload) {
-    return new Response(JSON.stringify({ error: "Token invalido" }), {
+    return null;
+  }
+
+  return payload;
+}
+
+
+export async function GET(request: Request): Promise<Response> {
+
+  const token = verifyToken(request);
+  const payload = await getPayload(token);
+
+   if (!payload) {
+    return new Response(JSON.stringify({ error: "Token inválido" }), {
       status: 401,
     });
   }
   
-  // Obtener el usuario 
   const user = await prisma.user.findUnique({
     where: { username: payload.username },
     include: { tasks: true } 
   });
 
-  // Si el usuario no es válido, retornar error 404 
   if (!user) {
     return new Response(JSON.stringify({ error: "Usuario no encontrado" }), {
       status: 404,
     });
   }
 
-  // Obtener las tareas del usuario autenticado
-  // const tasks = await prisma.task.findMany({
-  //   orderBy: { createdAt: 'desc' },
-  // });
-
-
   return Response.json(user.tasks);
-}
+};
 
-export async function POST(request) {
-  // Verificar el token de autorización 
-  const authHeader = request.headers.get('authorization');
-  const token = authHeader?.split(' ')[1];
 
-  // Si no hay token, retornar error 401 
-  const payload = await verifyJWT(token);
-  if (!payload) {
-    return new Response(JSON.stringify({ error: "No autorizado" }), {
+export async function POST(request: Request): Promise<Response> {
+    
+  const token = verifyToken(request);
+  const payload = await getPayload(token);
+
+ if (!payload) {
+    return new Response(JSON.stringify({ error: "Token inválido" }), {
       status: 401,
     });
   }
-
   
-  // Verificar el token y obtener el usuario 
   const user = await prisma.user.findUnique({
     where: { username: payload.username },
   });
 
 
-  // Si no hay usuario, retornar error 403 
   if (!user) {
     return new Response(JSON.stringify({ error: "Usuario no encontrado" }), {
       status: 404,
     });
   }
 
-  // Obtener los datos de la solicitud 
   const data = await request.json();
 
   if (!data.title || typeof data.title !== 'string') {
@@ -79,7 +81,6 @@ export async function POST(request) {
     });
   }
 
-  // Crear la nueva tarea en la base de datos 
   const task = await prisma.task.create({
     data: {
       title: data.title,
@@ -92,4 +93,4 @@ export async function POST(request) {
   });
 
   return Response.json(task, { status: 201 });
-}
+};
